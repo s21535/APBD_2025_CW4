80c6dbb7e4be2604ff88764bcaf4617494cf29c0
Maciej Litwinowicz <s21535@pjwstk.edu.pl>
2025-04-13 22:14:21 +0200

    Added Animal Model

---
+++ RESTApplication/RESTApplication/Models/Animal.cs
@@ -0,0 +1,10 @@
+namespace RESTApplication.Models;
+
+public class Animal
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Category { get; set; }
+    public float Mass { get; set; }
+    public string Color { get; set; }
+}
