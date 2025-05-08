bfa4ac214d64a0d7d34cc3314c1841eed3e651da
Maciej Litwinowicz <s21535@pjwstk.edu.pl>
2025-05-08 19:37:02 +0200

    Dodano bazową listę ze zwierzetami

--- RESTApplication/RESTApplication/Controllers/AnimalsController.cs
+++ RESTApplication/RESTApplication/Controllers/AnimalsController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RESTApplication.Models;
 
 namespace RESTApplication.Controllers
 {
@@ -7,11 +8,55 @@ namespace RESTApplication.Controllers
     [ApiController]
     public class AnimalsController : ControllerBase
     {
+        List<Animal> animals = new List<Animal>
+        {
+            new Animal()
+            {
+                Id = 1,
+                Name = "Psotka",
+                Category = "Pies",
+                Mass = 2.5f,
+                Color = "Brązowy"
+            },
+            new Animal()
+            {
+                Id = 2,
+                Name = "Mamrotek",
+                Category = "Kot",
+                Mass = 2.1f,
+                Color = "Czarny"
+            },
+            new Animal()
+            {
+                Id = 3,
+                Name = "Lenny",
+                Category = "Ryba",
+                Mass = 0.5f,
+                Color = "Zielony"
+            },
+            new Animal()
+            {
+                Id = 4,
+                Name = "Jerry",
+                Category = "Mysz",
+                Mass = 0.2f,
+                Color = "Brązowy"
+            },
+            new Animal()
+            {
+                Id = 5,
+                Name = "Miluś",
+                Category = "Pies",
+                Mass = 10.5f,
+                Color = "Biały"
+            }
+        };
+        
         //GET api/animals
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok("Hello World!");
+            return Ok(animals);
         }
     }
 }
